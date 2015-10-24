f89cdce9b432f09cfe7c1621ee1de260f90ab4ac
Smart Developer <ddeveloperr@gmail.com>
2015-10-24 06:06:24 +0000

    Complete example 29

--- ex28.rb
+++ ex28.rb
@@ -1,4 +1,4 @@
-true & true
+true && true
 false && true
 1 == 1 && 2 == 1
 "test" == "test"
@@ -8,14 +8,13 @@ false && 0 != 0
 true || 1 == 1
 "test" == "testing"
 1 != 0 && 2 == 1
-"test" != "testing" 
+"test" != "testing"
 "test" == 1
 !(true && false)
 !(1 == 1 && 0 != 1)
 !(10 == 1 || 1000 == 1000)
 !(1 != 10 || 3 == 4)
 !("testing" == "testing" && "Zed" == "Cool Guy")
-1 == 1 && (!("testing" == 1 || 1 ==0))
+1 == 1 && (!("testing" == 1 || 1 == 0))
 "chunky" == "bacon" && (!(3 == 4 || 3 == 3))
-3 == 3 && (("testing" || "Ruby" == "Fun"))
-
+3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))
