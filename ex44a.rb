60d410a5248e839a011a2e43858fcda1a0b66f87
Kemal C <ddeveloperr@gmail.com>
2015-11-07 20:00:22 +0000

    Ex 44a - Implicit Inheritance

---
+++ ex44a.rb
@@ -0,0 +1,16 @@
+class Parent
+    def implicit()
+        puts "PARENT implicit()"
+    end
+end
+
+class Child < Parent
+end
+
+dad = Parent.new()
+son = Parent.new()
+
+dad.implicit()
+son.implicit()
+
+
