68f583cbb908d3e86511dff8ae052482e022980d
Kemal C <ddeveloperr@gmail.com>
2016-01-29 20:57:40 +0000

    how to deal with Instance methods

---
+++ simple_bank_account.rb
@@ -0,0 +1,18 @@
+## Demonstrate how to deal with Instance methods
+
+class BankAccount
+    
+    def initialize(first_name, last_name)
+        @balance = 0
+        @first_name = first_name
+        @last_name = last_name
+    end
+    
+    def deposit(amount)
+        @balance += amount
+    end
+    
+    def withdraw(amount)
+        @balance -= amount
+    end
+end
